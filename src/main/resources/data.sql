b792939e1f8bc38919af47b63dfc88b79c528ef1
Ayush <hackerboy5556@gmail.com>
2025-02-05 04:26:37 +0530

    add sql to populate the initial database

---
+++ src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO todos (title, description, completed) VALUES
+('Buy groceries', 'Milk, Bread, Cheese, Eggs', FALSE),
+('Read a book', 'Finish reading "The Great Gatsby"', FALSE),
+('Workout', 'Go for a 30-minute run', FALSE);
